60b9d693d13c00733dcbf999a33e54f2e5f82ede
Everton Dutra <evertondutra02@gmail.com>
2020-07-30 00:13:00 -0300

    Front end com Banco de dados ok

--- index.py
+++ index.py
@@ -1,43 +1,47 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, Frame
 from tkinter import ttk
 import DataBaser
 
 # Criar janela
 jan = Tk()
 jan.title('Painel de Acesso')
-jan.geometry('600x300')#o tamanho da janela
-jan.configure(background='orange')#Configura a cor da janela
-jan.resizable(width=False, height=False)# impede que o tamanho da janela seja alterado
-jan.attributes('-alpha', 0.9)# deixando um pouco transparente
-jan.iconbitmap(default='icons/Logoicon.ico')# Adiciona o logo icon no canto superior esquerdo
+jan.geometry('300x600')  # o tamanho da janela
+jan.configure(background='white')  # Configura a cor da janela
+jan.resizable(width=False, height=False)  # impede que o tamanho da janela seja alterado
+jan.attributes('-alpha', 0.9)  # deixando um pouco transparente
+jan.iconbitmap(default='icons/Logoicon.ico')  # Adiciona o logo icon no canto superior esquerdo
 
 # Carregar imagem
-logo = PhotoImage(file='icons/jam.png')#imagem (100x100)
+logo = PhotoImage(file='icons/jam.png')  # imagem (100x100)
 
 # Criar os widgets da janela separando em dois, direita e esquerda
 # separada por uma barra branca
-LeftFrame = Frame(jan, width=200, height=300, bg='GREEN', relief='raise')
-LeftFrame.pack(side=LEFT)
+UpFrame: Frame = Frame(jan, width=300, height=200, bg='GREEN', relief='raise')
+UpFrame.pack(side=TOP)
 
-RightFrame = Frame(jan, width=395, height=300, bg='MIDNIGHTBLUE', relief='raise')
-RightFrame.pack(side=RIGHT)
-#bg é a cor de fundo
-LogoLabel = Label(LeftFrame, image=logo, bg='GREEN')#Exibi um texto ou imagem
-LogoLabel.place(x=50, y=100)# posicionando a imagem
+LowFrame = Frame(jan, width=300, height=395, bg='MIDNIGHTBLUE', relief='raise')
+LowFrame.pack(side=RIGHT)
+# bg é a cor de fundo
+LogoLabel = Label(UpFrame, image=logo, bg='GREEN')  # Exibi um texto ou imagem
+LogoLabel.place(x=100, y=50)  # posicionando a imagem
 # fg é a cor do texto
-UserLabel = Label(RightFrame, text='Nome do Usuário:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
-UserLabel.place(x=5, y=110)
 
-UserEntry = ttk.Entry(RightFrame, width=30)
-UserEntry.place(x=160, y=112)
+UserLabel = Label(LowFrame, text='Usuário:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
+UserLabel.place(x=5, y=150)
 
+UserEntry = ttk.Entry(LowFrame, width=30)
+UserEntry.place(x=100, y=152)
 
-PassLabel = Label(RightFrame, text='Senha:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
-PassLabel.place(x=5, y=155)
+PassLabel = Label(LowFrame, text='Senha:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
+PassLabel.place(x=5, y=185)
 
-PassEntry = ttk.Entry(RightFrame, width=30, show='*')#width = tamanho máximo do texto, show = mostra * no lugar digitado
-PassEntry.place(x=160, y=157)
+PassEntry = ttk.Entry(LowFrame, width=30, show='*')  # width = tmanho máximo do texto, show = mostra * no lugar digitado
+PassEntry.place(x=100, y=187)
+
+login_label = Label(LowFrame, text='Faça seu Login', font=('Century Gothic', 16), bg='MIDNIGHTBLUE', fg='yellow')
+login_label.place(x=5, y=15)
+
 
 def login():
     user = UserEntry.get()
@@ -50,7 +54,7 @@ def login():
     print('Selecionado')
     verifylogin = DataBaser.cursor.fetchone()
     try:
-        if (user in verifylogin and senha in verifylogin):
+        if user in verifylogin and senha in verifylogin:
             messagebox.showinfo(title='Login Info', message='Login Efetuado com Sucesso.')
     except:
         messagebox.showerror(title='Login info', message='Acesso Negado')
@@ -61,20 +65,20 @@ def register():
     loginbuttom.place(x=5000)
     cadbuttom.place(x=5000)
     # Inserindo Widgets de cadastro
-    nomelabel = Label(RightFrame, text='Nome:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
-    nomelabel.place(x=5, y=20)
-    nomeentry = ttk.Entry(RightFrame, width=30)
-    nomeentry.place(x=160, y=22)
+    nomelabel = Label(LowFrame, text='Nome:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
+    nomelabel.place(x=5, y=80)
+    nomeentry = ttk.Entry(LowFrame, width=30)
+    nomeentry.place(x=100, y=82)
 
-    EmailLabel = Label(RightFrame, text='Email:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
-    EmailLabel.place(x=5, y=65)
-    EmailEntry = ttk.Entry(RightFrame, width=30)
-    EmailEntry.place(x=160, y=67)
+    emaillabel = Label(LowFrame, text='Email:', font=('Century Gothic', 12), bg='MIDNIGHTBLUE', fg='White')
+    emaillabel.place(x=5, y=115)
+    emailentry = ttk.Entry(LowFrame, width=30)
+    emailentry.place(x=100, y=117)
 
-    def RegisterBD():
+    def registerbd():
         nome = nomeentry.get()
         usuario = UserEntry.get()
-        email = EmailEntry.get()
+        email = emailentry.get()
         senha = PassEntry.get()
         if nome == '' or usuario == '' or email == '' or senha == '':
             messagebox.showerror(title='ERRO Ao Registrar', message='Preencha Todos Os Campos Corretamente!')
@@ -85,35 +89,36 @@ def register():
             DataBaser.conexao.commit()
             messagebox.showinfo(title='Registro da Informação', message='Registrado com Sucesso')
 
-
     def voltar():
-        #Removendo width de registro
-        nomelabel.place(x=5000)
-        nomeentry.place(x=5000)
-        EmailLabel.place(x=5000)
-        EmailEntry.place(x=5000)
-        voltar.place(x=5000)
-        registrar.place(x=5000)
-        #Inserindo width de button
-        passentry = ttk.Entry(RightFrame, width=30, show='*')  # width = tamanho máximo do texto, show = mostra * no lugar digitado
-        passentry.place(x=160, y=157)
-        UserEntry = ttk.Entry(RightFrame, width=30)
-        UserEntry.place(x=160, y=112)
-        loginbuttom.place(x=160, y=210)
-        cadbuttom.place(x=254, y=210)
+        # Removendo width de registro
+        cadlabel.place_forget()
+        nomelabel.place_forget()
+        nomeentry.place_forget()
+        emaillabel.place_forget()
+        emailentry.place_forget()
+        voltar.place(x=2000)
+        registrar.place(x=2000)
+        # Inserindo width de button
+        loginlabel = Label(LowFrame, text='Faça seu Login', font=('Century Gothic', 16), bg='MIDNIGHTBLUE', fg='yellow')
+        loginlabel.place(x=5, y=15)
+        loginbuttom.place(x=120, y=225)
+        cadbuttom.place(x=10, y=355)
 
-    voltar = ttk.Button(RightFrame, text='Voltar', width=10, command=voltar)
-    voltar.place(x=160, y=210)
+    cadlabel = Label(LowFrame, text='Faça seu Cadastro', font=('Century Gothic', 16), bg='MIDNIGHTBLUE', fg='yellow')
+    cadlabel.place(x=5, y=15)
+    voltar = ttk.Button(LowFrame, text='Voltar', width=10, command=voltar)
+    voltar.place(x=10, y=355)
 
-    registrar = ttk.Button(RightFrame, text='Registrar', width=14, command=RegisterBD)
-    registrar.place(x=254, y=210)
+    registrar = ttk.Button(LowFrame, text='Registrar', width=14, command=registerbd)
+    registrar.place(x=110, y=225)
+
 
 # Botões
 
-loginbuttom = ttk.Button(RightFrame, text='Login', width=10, command=login)
-loginbuttom.place(x=160, y=210)
+loginbuttom = ttk.Button(LowFrame, text='Login', width=10, command=login)
+loginbuttom.place(x=120, y=225)
 
-cadbuttom = ttk.Button(RightFrame, text='Cadastre-se', width=14, command=register)
-cadbuttom.place(x=254, y=210)
+cadbuttom = ttk.Button(LowFrame, text='Cadastre-se', width=14, command=register)
+cadbuttom.place(x=10, y=355)
 
 jan.mainloop()
